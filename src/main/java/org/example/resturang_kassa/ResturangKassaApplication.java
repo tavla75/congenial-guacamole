593d8a1f901d37d037158281d12da9c682cfcfdb
tavla75 <joseph.lindetun@gmail.com>
2026-09-24 15:36:15 +0200

    first commit

---
+++ src/main/java/org/example/resturang_kassa/ResturangKassaApplication.java
@@ -0,0 +1,13 @@
+package org.example.resturang_kassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturangKassaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResturangKassaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/resturang_kassa/ResturangKassaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resturang_kassa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResturangKassaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
